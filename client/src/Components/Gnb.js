67c24f3f8aee1ff51fd3b3c4f4db57e408582f81
DynePark2022 <parkdyne2022@gmail.com>
2022-01-03 12:57:32 +0900

    before refactoring

--- client/src/Components/Gnb.js
+++ client/src/Components/Gnb.js
@@ -11,16 +11,16 @@ function Gnb() {
             url: "webtoon?toon=ongoing",
             type: "TAB_ONGOING",
         },
-        { id: 1, name: "성인", url: "webtoon?toon=adult", type: "TAB_ADULT" },
-        { id: 2, name: "BL/GL", url: "webtoon?toon=bl%2Fgl", type: "TAB_BLGL" },
         {
-            id: 3,
+            id: 1,
             name: "완결",
             url: "webtoon?toon=completed",
             type: "TAB_COMPLETED",
         },
-        { id: 4, name: "My", url: "user", type: "TAB_MY" },
-        { id: 5, name: "커뮤니티", url: "community", type: "TAB_COMMUNITY" },
+        { id: 2, name: "성인", url: "webtoon?toon=adult", type: "TAB_ADULT" },
+        // { id: 3, name: "BL/GL", url: "webtoon?toon=bl%2Fgl", type: "TAB_BLGL" },
+        { id: 4, name: "My*", url: "user", type: "TAB_MY" },
+        { id: 5, name: "커뮤니티*", url: "community", type: "TAB_COMMUNITY" },
     ];
     const tab = useSelector((state) => state.reducerTab);
     let dispatch = useDispatch();
