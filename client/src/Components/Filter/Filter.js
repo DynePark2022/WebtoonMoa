6f60781553a0c0c744fb62af6e7add95ca637427
DynePark2022 <parkdyne2022@gmail.com>
2022-01-16 19:07:30 +0900

    Feat FE filter error

--- client/src/Components/Filter/Filter.js
+++ client/src/Components/Filter/Filter.js
@@ -4,19 +4,17 @@ import { FilterArray } from "../../DB/text";
 
 function Filter() {
     const [platform, setPlatform] = useState("전체");
-    const [day, setDay] = useState("전체");
-    const [genre, setGenre] = useState("전체");
-    const [age, setAge] = useState("전체");
-    const [consonant, setConsonant] = useState("전체");
-
-    // platform:"전체",
-    // day:"전체",
-    // genre:"전체",
-    // age:"전체",
-    // consonant:"전체",
 
     //FIXME: change to one useState
 
+    const [tags, setTags] = useState({
+        연재: "전체",
+        요일: "전체",
+        장르: "전체",
+        나이: "전체",
+        초성: "전체",
+    });
+    console.log(tags);
     return (
         <div className={styles.filter}>
             <form className={styles.filter_container}>
@@ -26,9 +24,8 @@ function Filter() {
                         {item.list.map((tag) => (
                             <ul key={tag}>
                                 <li
-                                    onClick={() => setPlatform(tag)}
                                     id={
-                                        platform === tag
+                                        tags.연재 === tag
                                             ? `${styles.selected}`
                                             : ""
                                     }
@@ -46,3 +43,39 @@ function Filter() {
 }
 
 export default Filter;
+
+// () => setTags({ ...tags })
+
+// onClick={
+//     item.title === "연재"
+//         ? () =>
+//               setTags({
+//                   ...tags,
+//                   연재: tag,
+//               })
+//         : item.title === "요일"
+//         ? () =>
+//               setTags({
+//                   ...tags,
+//                   요일: tag,
+//               })
+//         : item.title === "장르"
+//         ? () =>
+//               setTags({
+//                   ...tags,
+//                   장르: tag,
+//               })
+//         : item.title === "나이"
+//         ? () =>
+//               setTags({
+//                   ...tags,
+//                   나이: tag,
+//               })
+//         : item.title === "초성"
+//         ? () =>
+//               setTags({
+//                   ...tags,
+//                   초성: tag,
+//               })
+//         : () => setTags({ ...tags })
+// }
